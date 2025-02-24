0e9a7a70c360018dcb8ad4e7a86cfcc0fa3102ba
Yuliia Skrypnyk <jillyjoolli@gmail.com>
2025-02-24 14:34:14 +0100

    Initial commit

---
+++ src/main/java/com/asterix/api/springrecapproject/SpringRecapProjectApplication.java
@@ -0,0 +1,13 @@
+package com.asterix.api.springrecapproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRecapProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRecapProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asterix/api/springrecapproject/SpringRecapProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asterix.api.springrecapproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRecapProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
